a8a4796eb369de1ad8761d45b973ecc703b8849e
Bradley Zazzara <bradleyzazz@gmail.com>
2020-06-10 13:34:22 -0700

    Add Camera functionality and text awareness

--- app/Camera.js
+++ app/Camera.js
@@ -1,13 +1,61 @@
-import * as React from 'react';
-import { Text, StyleSheet, SafeAreaView, Button } from 'react-native';
+import React, { PureComponent } from 'react';
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import RNTextDetector from "react-native-text-detector";
 
 const Camera = ({ navigation }) => {
+
+  const takePicture = async () => {
+    if (camera) {
+      const options = { quality: 0.9, base64: true };
+      const { uri } = await camera.takePictureAsync(options);
+      console.log(uri);
+      const visionResp = await RNTextDetector.detectFromUri(uri);
+      console.log('visionResp', visionResp);
+    }
+  }
+
   return (
-    <SafeAreaView>
-      <Text>Camera</Text>
-      <Button title="Crop" onPress={() => navigation.navigate('Crop')} />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <RNCamera
+        ref={(ref) => {
+          camera = ref;
+        }}
+        style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.auto}
+        captureAudio={false}
+        onTextRecognized={({textBlocks}) => console.log(textBlocks)} 
+      />
+      <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+        <TouchableOpacity onPress={takePicture} style={styles.capture}>
+          <Text style={{ fontSize: 14 }}> SNAP </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
-};
+  }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
 
 export default Camera;
